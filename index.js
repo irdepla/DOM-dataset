fb7e32c440ea9d076e3a59b687c7c158399ad263
irdepla <rixsiboyevislombek@gmail.com>
2024-11-05 20:12:38 +0500

    all exercises solved

---
+++ index.js
@@ -0,0 +1,57 @@
+// Exercise 2
+
+// const arr = [1,2,3,4,5,6,7,8,9];
+// let resArray = []
+
+// function randomArray(array, num) {
+//     for (let i = 0; i < num; i++) {
+//         const r = parseInt(Math.random() * array.length);
+//         resArray.push(array[r])
+//     }
+//     return resArray;
+// }
+
+
+// console.log(randomArray(arr, 3));
+
+
+
+// Exercise 3
+
+
+// let arr = ["apple", "banana", "apple", "orange", "banana"];
+
+// let arraySet = new Set(arr);
+// let newArray = [...arraySet];
+// console.log(newArray);
+
+
+
+// Exercise 4
+
+
+// const arr = [5, [6,7], 8,9, [14,36,12,5]];
+
+// const newArr = arr.flat()
+// console.log(newArr);
+
+
+
+
+// Exercise 5
+
+
+// let str = "hello world from javascript"
+// let arr = str.split(" ")
+
+
+
+// let newArr = arr.map(function (word) {
+//     let firstLetter = word[0].toUpperCase();
+//     let newWord = firstLetter + word.slice(1);
+//     return newWord;
+// })
+
+// let t = newArr.join(" ")
+
+// console.log(t);
